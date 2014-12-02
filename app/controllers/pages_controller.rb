5644bd9cfd3a42b11b7b92a59b3557df05288fe3
joesiewert <joe.siewert@gmail.com>
2014-12-02 13:17:43 -0700

    Completed exercise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,16 @@
 class PagesController < ApplicationController
 
+  before_action :authorize
+
+  def index
+  end
+
+  private
+
+  def authorize
+    if session[:user_id] != 3
+      raise AccessDenied
+    end
+  end
+
 end
